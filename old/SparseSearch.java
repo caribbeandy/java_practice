f97c4304e39e83f9215fe0fd94ee9b4f35d4deea
drew <a9palmer@uwaterloo.ca>
2017-04-22 23:29:38 -0400

    Cleanup

--- old/SparseSearch.java
+++ old/SparseSearch.java
@@ -1,69 +1,69 @@
 class SparseSearch {
 
-	public static void main(String [] args) {
+    public static void main(String [] args) {
 
         String [] s = new String[]{"at", "", "","","ball","","","car","","","dad","",""};
         System.out.println(search(s, "ball",0,s.length-1));
 
-	}
+    }
 
-	public static int search(String [] s, String needle, int start, int end) {
+    public static int search(String [] s, String needle, int start, int end) {
 
-		// base case
-		if (end < start) {
-			return -1;
-		}
+        // base case
+        if (end < start) {
+            return -1;
+        }
 
-		int mid = (start+end)/2;
+        int mid = (start+end)/2;
 
-		// we're done
-		if ( s[mid].equals(needle) ) {
-			return mid;
+        // we're done
+        if ( s[mid].equals(needle) ) {
+            return mid;
 
-			// On the left
-		} else {
-			int onTheLeft = onTheLeft(s,needle,start,mid,end);
+            // On the left
+        } else {
+            int onTheLeft = onTheLeft(s,needle,start,mid,end);
             System.out.println(mid + " " + onTheLeft);
-			if ( onTheLeft == 1 ) {
-				return search(s,needle,start,mid-1);
+            if ( onTheLeft == 1 ) {
+                return search(s,needle,start,mid-1);
 
-				// On the right
-			} else if (onTheLeft == -1) {
-				return search(s,needle,mid+1,end);
-			} else {
-				return -1;
-			}
-		}
+                // On the right
+            } else if (onTheLeft == -1) {
+                return search(s,needle,mid+1,end);
+            } else {
+                return -1;
+            }
+        }
 
-	}
+    }
 
-	public static int onTheLeft(String [] s, String needle, int start,int mid, int end) {
+    public static int onTheLeft(String [] s, String needle, int start,int mid, int end) {
 
-		// check left
-		int i = 1;
+        // check left
+        int i = 1;
 
-		for(; i<=mid-start; i++){
+        for(; i<=mid-start; i++){
 
-			if ( (mid-i>=0) && !s[mid-i].equals("") ) {
+            if ( (mid-i>=0) && !s[mid-i].equals("") ) {
                 if ( s[mid-i].compareTo(needle) < 0 ) {
-				    return -1;
+                    return -1;
                 } else {
                     return 1;
                 }
-			}
+            }
 
-			if ( (mid+i <= end) && !s[mid+i].equals("") ) {
-            
+            if ( (mid+i <= end) && !s[mid+i].equals("") ) {
+
                 if ( s[mid+i].compareTo(needle) < 1 ) {
                     return -1;
                 } else {
                     return 1;
                 }
-			}
+            }
 
-		}
+        }
 
-		return 0;
-	}
+        return 0;
+    }
 }
 
